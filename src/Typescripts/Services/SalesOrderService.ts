e30770937d951f44b42d7ae4abeac251c0e4fe3d
dariodds <dario.dasilva.private@gmail.com>
2025-02-12 14:10:57 +0100

    refactor service functions

--- src/Typescripts/Services/SalesOrderService.ts
+++ src/Typescripts/Services/SalesOrderService.ts
@@ -7,37 +7,21 @@ export class SalesOrderService {
   /**
    * Set fulfillment data on all item line base on header data
    * inventory location column is set with header location value
-   * inventory subsidiary column is set with header subsidiary value
    * @param so Sales order instance representing the related transaction
    */
   setFulfillmentLocationOnItemLines(so: SalesOrder) {
-    const headerLocation = so.location;
-    const headerSubsidiary = so.subsidiary;
-
-    log.debug('SalesOrderService : setFulfillmentLocationOnCurrentItemLine', `header location: ${headerLocation}`);
-    log.debug('SalesOrderService : setFulfillmentLocationOnCurrentItemLine', `header subsidiary: ${headerSubsidiary}`);
-
     so.item.entries.forEach((item: SalesOrderItemSublist) => {
-      log.debug('SalesOrderService : setFulfillmentLocationOnItemLines', `location before: ${item.inventorylocation}`);
-      log.debug('SalesOrderService : setFulfillmentLocationOnItemLines', `subsidiary before: ${item.inventorysubsidiary}`);
-      item.inventorylocation = headerLocation;
-      item.inventorysubsidiary = headerSubsidiary;
-      log.debug('SalesOrderService : setFulfillmentLocationOnItemLines', `location after: ${item.inventorylocation}`);
-      log.debug('SalesOrderService : setFulfillmentLocationOnItemLines', `subsidiary after: ${item.inventorysubsidiary}`);
+      item.inventorylocation = so.location;
     });
   }
 
   /**
    * Set fulfillment data on current item line base on header data
    * inventory location column is set with header location value
-   * inventory subsidiary column is set with header subsidiary value
    * @param so Sales order instance representing the related transaction
    */
   setFulfillmentLocationOnCurrentItemLine(so: SalesOrder) {
     const currIndex = so.item.getCurrentSublistIndex();
-    const headerLocation = so.location;
-
-    so.item[currIndex].inventorylocation = headerLocation;
-    return `Inventory location ${headerLocation} was set on line index ${currIndex}`;
+    so.item[currIndex].inventorylocation = so.location;
   }
 }
